bab769892c659ea2f28279b1cc4c0a7c07906af3
hi101000 <60079731+hi101000@users.noreply.github.com>
2020-11-27 11:21:38 -0500

    Add files via upload

--- MyOS/build.sh
+++ MyOS/build.sh
@@ -2,3 +2,5 @@ nasm -f elf32 kernel.asm -o ksm.o
 gcc -m32 -c kernel.c -o kc.o
 ld -m elf_i386 -T link.ld -o kernel.bin ksm.o kc.o
 qemu-system-i386 -kernel kernel.bin
+grub-mkrescue -o myos.iso MyOS/
+clear
